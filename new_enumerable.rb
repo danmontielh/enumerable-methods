a43b2c0d2779e88953f0b8504f02c5b49898b3c4
Dan Montiel <danmontielh@gmail.com>
2019-05-04 16:58:20 -0500

    count, any, noun

--- new_enumerable.rb
+++ new_enumerable.rb
@@ -28,6 +28,41 @@ module Enumerable
     returnedArray
     end #method
 
+    def my_all?
+        returnedArray = []
+        self.my_each do |element|
+            if block_given?
+                if yield(element) == true
+                    returnedArray.push(element)
+                end  
+            end
+        end
+        self.size == returnedArray.size ? (return true) : (return false)
+    end
+
+    def my_any?
+        self.my_each do |element|
+            return true if yield element
+            false
+        end
+    end
+
+    def my_none?
+        self.my_each {|element| return false if yield(element)}
+        true
+    end
+
+    def my_count(arg = nil)
+        count = 0
+        if !block_given? && !arg
+            puts self.length
+        elsif !block_given? && arg
+            self.my_each { |element| count +=1 if element == arg }
+        elsif block_given?
+            self.my_each { |element| count+=1 if yield(element) }
+        end
+        count
+    end
 
 
 end #Enumerable
@@ -37,6 +72,10 @@ arr = [25,45,55,36]
 # Test cases
 # arr.my_each {|n| p  n * 2 }
 
-arr.my_each_with_index {|element, index| p "Index #{index}, elemento #{element}"   }
+#arr.my_each_with_index {|element, index| p "Index #{index}, elemento #{element}"   }
 
-p ["hola", "soy", "juan"].my_select {|letter| letter.size == 4 }
+#p ["hola", "soy", "juan"].my_select {|letter| letter.size == 4 }
+
+#p [5,6, false].my_any? { |n| n > 3 }
+
+# p [1,2,3,2,2,2,2].my_count { |n| n % 2 == 0 }
